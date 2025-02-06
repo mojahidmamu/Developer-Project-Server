7a301e6236a991b0639b718fd16725c6efcc926c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-06 07:55:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("boss in city");
+});
+
+app.listen(port, () => {
+  console.log(`bistro boss in the city in dhaka ${port}`);
+});
